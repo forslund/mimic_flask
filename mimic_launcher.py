128e1985b588385760a3007629f54c2eb8c8d13f
Åke Forslund <ake.forslund@gmail.com>
2016-12-17 15:33:44 +0100

    Add support for all voices in backend.

--- mimic_launcher.py
+++ mimic_launcher.py
@@ -13,7 +13,22 @@ MIMIC_DIR = '/home/ake/projects/c/mimic/'
 
 voices = {
     'awb': MIMIC_DIR + 'voices/cmu_us_awb.flitevox',
-    'mycroft': MIMIC_DIR + 'voices/mycroft_voice_4.0.flitevox'
+    'aew': MIMIC_DIR + 'voices/cmu_us_aew.flitevox',
+    'ahw': MIMIC_DIR + 'voices/cmu_us_ahw.flitevox',
+    'aup': MIMIC_DIR + 'voices/cmu_us_aup.flitevox',
+    'axb': MIMIC_DIR + 'voices/cmu_us_axb.flitevox',
+    'bdl': MIMIC_DIR + 'voices/cmu_us_bdl.flitevox',
+    'clb': MIMIC_DIR + 'voices/cmu_us_clb.flitevox',
+    'eey': MIMIC_DIR + 'voices/cmu_us_eey.flitevox',
+    'fem': MIMIC_DIR + 'voices/cmu_us_fem.flitevox',
+    'gka': MIMIC_DIR + 'voices/cmu_us_gka.flitevox',
+    'jmk': MIMIC_DIR + 'voices/cmu_us_jmk.flitevox',
+    'ksp': MIMIC_DIR + 'voices/cmu_us_ksp.flitevox',
+    'ljm': MIMIC_DIR + 'voices/cmu_us_ljm.flitevox',
+    'rms': MIMIC_DIR + 'voices/cmu_us_rms.flitevox',
+    'rxr': MIMIC_DIR + 'voices/cmu_us_rxr.flitevox',
+    'slt': MIMIC_DIR + 'voices/cmu_us_slt.flitevox',
+    'ap': MIMIC_DIR + 'voices/mycroft_voice_4.0.flitevox'
 }
 
 @app.route('/wav/<voice>/<sentence>')
